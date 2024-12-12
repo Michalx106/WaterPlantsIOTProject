cd91d8df466cae881ce85e6011c73ace5e9aaa18
Michał Grzesiewicz <michalx106@outlook.com>
2024-12-12 21:33:01 +0100

    dziala Blynk

--- PIR-Main-Board/src/main.cpp
+++ PIR-Main-Board/src/main.cpp
@@ -1,5 +1,5 @@
-#include "ButtonManager.hpp"
 #include "LCDManager.hpp"
+#include "ButtonManager.hpp"
 #include "PumpManager.hpp"
 #include "SensorDataManager.hpp"
 #include <esp_now.h>
@@ -8,29 +8,64 @@
 #define BUTTON_PIN 23
 #define PUMP_PIN 5
 
+#define BLYNK_TEMPLATE_ID "TMPL4lrKLMEOn"
+#define BLYNK_TEMPLATE_NAME "PIR centralka"
+#include <BlynkSimpleEsp32.h>
+
 LCDManager lcd(0x27, 16, 2);
 ButtonManager button(BUTTON_PIN);
 PumpManager pump(PUMP_PIN);
 SensorDataManager sensorData;
 
-static unsigned long lastReceivedTime = 0;
+// Dane logowania do Blynk
+char auth[] = "sb2Uqu8duDhkVwn1YDFb0PjmmZv2fWb0";
+char ssid[] = "Eldorado";
+char pass[] = "Kowieski08110";
 
+// Inicjalizacja globalnego obiektu Blynk
+BlynkTimer timer;
+
+// Funkcja odbierająca dane z ESP-NOW
 void onDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len) {
     SensorDataManager data;
     memcpy(&data, incomingData, sizeof(SensorDataManager));
     sensorData.update(data.temperature, data.humidity, data.soilMoisture, data.voltage);
-    lastReceivedTime = millis();
+}
+
+// Funkcja Blynk: Uruchomienie podlewania z aplikacji
+BLYNK_WRITE(V1) {
+    int value = param.asInt();
+    if (value == 1 && !pump.isRunning()) {
+        lcd.clear();
+        lcd.printMessage("Blynk Watering");
+        pump.start();
+    }
+}
+
+// Funkcja wysyłająca dane do Blynk
+void sendSensorDataToBlynk() {
+    Blynk.virtualWrite(V2, sensorData.temperature);
+    Blynk.virtualWrite(V3, sensorData.humidity);
+    Blynk.virtualWrite(V4, sensorData.soilMoisture);
+    Blynk.virtualWrite(V5, sensorData.voltage);
 }
 
 void setup() {
     Serial.begin(115200);
+
+    // Inicjalizacja LCD
     lcd.init();
+    lcd.printMessage("Ready!");
+
+    // Inicjalizacja przycisku i pompy
     button.init();
     pump.init();
 
+    // Ustawienie progów alarmowych
     sensorData.setFrostThreshold(-2);
     sensorData.setDrySoilThreshold(25);
 
+    // Inicjalizacja WiFi i ESP-NOW
     WiFi.mode(WIFI_STA);
     if (esp_now_init() != ESP_OK) {
         Serial.println("Error initializing ESP-NOW");
@@ -38,10 +73,15 @@ void setup() {
         esp_now_register_recv_cb(onDataRecv);
     }
 
-    lcd.printMessage("Ready!");
+    // Inicjalizacja Blynk
+    Blynk.begin(auth, ssid, pass);
+    timer.setInterval(2000L, sendSensorDataToBlynk); // Wysyłanie danych co 2 sekundy
 }
 
 void loop() {
+    Blynk.run();
+    timer.run();
+
     unsigned long pressDuration = button.getPressDuration();
 
     if (pressDuration > 0) {
@@ -81,24 +121,6 @@ void loop() {
                             String(sensorData.voltage, 1) + "V";
 
         lcd.scrollText(scrollText, 0);
-
-        if (millis() - lastReceivedTime > 10000) {
-            lcd.printMessage("No data!", 1);
-        } else if (sensorData.isFrostAlert()) {
-            if (millis() % 1000 < 500) {
-                lcd.printMessage("Frost Alert!", 1);
-            } else {
-                lcd.printMessage("                ", 1);
-            }
-        } else if (sensorData.isDrySoil()) {
-            if (millis() % 1000 < 500) {
-                lcd.printMessage("Dry Soil!", 1);
-            } else {
-                lcd.printMessage("                ", 1);
-            }
-        } else {
-            lcd.printMessage("                ", 1);
-        }
     }
 
     if (sensorData.isDrySoil() && !pump.isRunning()) {
@@ -106,6 +128,4 @@ void loop() {
         lcd.printMessage("Auto Watering", 1);
         pump.start();
     }
-
-    delay(100);
 }
