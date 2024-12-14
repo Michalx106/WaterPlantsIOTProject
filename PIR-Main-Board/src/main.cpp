8380e5c087d6ca60b193a7b664f6c6b26cb261a6
Michał Grzesiewicz <michalx106@gmail.com>
2024-12-14 13:06:21 +0100

    cd

--- PIR-Main-Board/src/main.cpp
+++ PIR-Main-Board/src/main.cpp
@@ -17,33 +17,28 @@ ButtonManager button(BUTTON_PIN);
 PumpManager pump(PUMP_PIN);
 SensorDataManager sensorData;
 
-// Dane logowania do Blynk
 char auth[] = "sb2Uqu8duDhkVwn1YDFb0PjmmZv2fWb0";
 char ssid[] = "Eldorado";
 char pass[] = "Kowieski08110";
 
-// Inicjalizacja globalnego obiektu Blynk
 BlynkTimer timer;
 
-// Funkcja odbierająca dane z ESP-NOW
 void onDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len) {
     SensorDataManager data;
     memcpy(&data, incomingData, sizeof(SensorDataManager));
     sensorData.update(data.temperature, data.humidity, sensorData.soilMoisture, sensorData.voltage);
 }
 
-// Funkcja Blynk: Uruchomienie podlewania z aplikacji
 BLYNK_WRITE(V1) {
     int value = param.asInt();
     if (value == 1 && !pump.isRunning()) {
-        Blynk.virtualWrite(V6, 1); // Wyślij status pompy do Blynk
+        Blynk.virtualWrite(V6, 1);
         lcd.clear();
         lcd.printMessage("Blynk Watering");
         pump.start();
     }
 }
 
-// Funkcja wysyłająca dane do Blynk
 void sendSensorDataToBlynk() {
     Blynk.virtualWrite(V2, sensorData.temperature);
     Blynk.virtualWrite(V3, sensorData.humidity);
@@ -51,7 +46,6 @@ void sendSensorDataToBlynk() {
     Blynk.virtualWrite(V5, sensorData.voltage);
 }
 
-// Funkcja aktualizująca status pompy w Blynk
 void updatePumpStatusInBlynk() {
     if (pump.isRunning()) {
         Blynk.virtualWrite(V6, 1); // Pompa włączona
@@ -63,15 +57,12 @@ void updatePumpStatusInBlynk() {
 void setup() {
     Serial.begin(115200);
 
-    // Inicjalizacja przycisku i pompy
     button.init();
     pump.init();
 
-    // Ustawienie progów alarmowych
     sensorData.setFrostThreshold(-2);
     sensorData.setDrySoilThreshold(25);
 
-    // Inicjalizacja WiFi i ESP-NOW
     WiFi.mode(WIFI_STA);
     if (esp_now_init() != ESP_OK) {
         Serial.println("Error initializing ESP-NOW");
@@ -79,12 +70,10 @@ void setup() {
         esp_now_register_recv_cb(onDataRecv);
     }
 
-    // Inicjalizacja Blynk
     Blynk.begin(auth, ssid, pass);
-    timer.setInterval(2000L, sendSensorDataToBlynk); // Wysyłanie danych co 2 sekundy
-    timer.setInterval(1000L, updatePumpStatusInBlynk); // Aktualizacja statusu pompy co 1 sekundę
+    timer.setInterval(2000L, sendSensorDataToBlynk);
+    timer.setInterval(1000L, updatePumpStatusInBlynk);
 
-    // Inicjalizacja LCD
     lcd.init();
     lcd.printMessage("Ready!");
 }
